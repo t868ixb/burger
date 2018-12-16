d0f8fb8433de14298d38996efc4ea21e3c684c07
“IBayon“ <IBayon@Enova.com>
2018-12-16 16:11:03 -0600

    fix delete route

--- config/orm.js
+++ config/orm.js
@@ -52,17 +52,17 @@ var orm = {
 
   }
 
-   // delete evidence()
+   //delete evidence()
    //did not finish this section!!!!!
-    // deleteEvidence: function(burgerID, callback){
+    deleteEvidence: function(burgerID, callback){
 
-  //  Run MySQL Query to delete the burger
-  // connection.query('DELETE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
-  //       if (err) throw err;
-  //       callback(result);
-  //      });
+   //Run MySQL Query to delete the burger
+  connection.query('DELETE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
+        if (err) throw err;
+        callback(result);
+       });
 
-  //  }
+   }
 
 
 }; //end of the orm object
